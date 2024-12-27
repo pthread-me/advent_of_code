df5c5a6455492f08a00d834a02c8299a7dae67dd
anas <anas.n.alhadi@outlook.com>
2024-12-27 01:33:36 -0400

    Big D Optimizations but still slower than the python alg that i dont understand. log10 is the bottleneck rn :(

--- 2024/day7/part2.cpp
+++ 2024/day7/part2.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <tokenizer.hpp>
 #include <vector>
+#include <cmath>
 
 bool check(long target, long sum, size_t i, const std::vector<long>& vec){
 	using namespace std;
@@ -14,10 +15,16 @@ bool check(long target, long sum, size_t i, const std::vector<long>& vec){
 	}else if(sum > target || i >= vec.size()){
 		return false;
 	}
-	return check(target, sum + vec.at(i), i+1,  vec) || 
-		check(target, sum * vec.at(i), i+1, vec) ||
-		check(target, stol(to_string(sum) + to_string(vec.at(i))) , i+1, vec);
-	
+
+	long next = vec.at(i);
+	long sum_len = log10(sum) +1;
+	long next_len = log10(next) +1;
+
+	bool concat = sum_len+next_len <= log10(target) +1;
+
+	return check(target, sum * vec.at(i), i+1,  vec) || 
+		check(target, sum + vec.at(i), i+1, vec) ||
+		(concat && check(target, (sum* pow(10,next_len)) + next, i+1, vec));
 }
 
 int main(){
